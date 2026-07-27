3c1ed2e693edaabb30521ea657a186fd8ef68d37
gamerplaylost-sys <gamerplaylost@gmail.com>
2026-07-27 16:49:51 +0500

    Update hug.tsx

--- src/routes/hug.tsx
+++ src/routes/hug.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, Link } from "@tanstack/react-router";
 import { ArrowRight } from "lucide-react";
 import { GlassNav } from "@/components/GlassNav";
 
-import f01 from "@/assets/hug/01.png";
+import f01 from "@/assets/hug/hug-01.png.asset.json";
 import f02 from "@/assets/hug/hug-02.png.asset.json";
 import f03 from "@/assets/hug/hug-03.png.asset.json";
 import f04 from "@/assets/hug/hug-04.png.asset.json";
@@ -29,7 +29,7 @@ import f22 from "@/assets/hug/hug-22.png.asset.json";
 const FRAMES = [
   f01, f02, f03, f04, f05, f06, f07, f08, f09, f10, f11,
   f12, f13, f14, f15, f16, f17, f18, f19, f20, f21, f22,
-].map((a) => a.png);
+].map((a) => a.url;
 
 const CAPTIONS = [
   { at: 0.06, text: "Come here." },
